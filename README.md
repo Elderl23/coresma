a59356cd2b01c3067aeac58c9b2009bb288daef1
Elder Lopez <lopez_felix_23@hotmail.com>
2020-09-20 22:31:06 -0500

    prueba de integracion v10

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Qualitas
+# coresma
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.7.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('qualitas app is running!');
+    expect(page.getTitleText()).toEqual('coresma app is running!');
   });
 
   afterEach(async () => {
